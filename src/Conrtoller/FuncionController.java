d3c5579b4db23c1597952f1f4ba0e42fc2f4f76c
KennethGranados <kennethgranados@gmail.com>
2021-02-13 14:09:49 -0600

    terminado

--- src/Conrtoller/FuncionController.java
+++ src/Conrtoller/FuncionController.java
@@ -8,31 +8,52 @@ package Conrtoller;
 import Views.FuncionesFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  *
  * @author Kenneth Granados
  */
-public class FuncionController implements ActionListener{
+public class FuncionController implements ActionListener, KeyListener {
+
     FuncionesFrame ff;
 
     public FuncionController(FuncionesFrame ff) {
         this.ff = ff;
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
-      switch(ae.getActionCommand()){
-         case "Graficar":
-              ff.getgrafica();
-             break;
-         case "Agregar":
-             ff.addgrafica();
-             break;
-         case "Limpiar":
-             ff.clean();
-             break;
-     }
+        switch (ae.getActionCommand()) {
+            case "Graficar":
+                ff.getgrafica();
+                break;
+            case "Agregar":
+                ff.addgrafica();
+                break;
+            case "Limpiar":
+                ff.clean();
+                break;
+        }
     }
-    
+
+    @Override
+    public void keyTyped(KeyEvent ke) {//recive evento del teclado y q anule espacio
+        if (ke.getSource() == ff.getFuncTextField() || ke.getSource() == ff.getIntervaloTextField() || ke.getSource() == ff.getX0TextField() || ke.getSource() == ff.getXnTextField()) {
+            if (ke.getKeyChar() == ' ') {
+                ke.consume();
+            }
+
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
+
 }
